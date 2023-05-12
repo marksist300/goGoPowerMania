7cd58edf7b62767bfd451922f98e4b87cc1e480a
Mark Linn <marktlinn@gmail.com>
2023-05-12 15:26:01 +0200

    add: basic functionality of cli quiz, part 1 complete

--- gophercises/quiz/main.go
+++ gophercises/quiz/main.go
@@ -11,37 +11,66 @@ import (
 
 var problemMap map[string]int = map[string]int{}
 
+// Insert everything into the map the Q's as strings; answers as Numbers
 func insertQuestions(probMap map[string]int, data []string) {
 	var sum, answer string = data[0], data[1]
-	num, err := strconv.Atoi(answer)
-	if err == nil {
-		probMap[sum] = num
-	} else {
-		fmt.Println("Error converting answer to number", err)
-	}
-
+	var number int = parseNums(answer)
+	probMap[sum] = number
 }
 
-func main() {
+// convert string number to int types
+func parseNums(strNum string) int {
+	num, err := strconv.Atoi(strNum)
+	if err != nil {
+		fmt.Printf("Error converting string %v to number\n", strNum)
+		fmt.Println(err)
+	}
+	return num
+}
+
+// Take in all the data from the CSV and parse it.
+func readInFileData(probMap map[string]int) {
 	file, err := os.Open("problems.csv")
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer file.Close()
-
 	csvRead := csv.NewReader(file)
 	for {
 		data, err := csvRead.Read()
 		if err == io.EOF {
-			fmt.Println("*** End of file ***")
 			break
 		}
 		if err != nil {
 			log.Fatal(err)
 		}
-		insertQuestions(problemMap, data)
-		// fmt.Printf("Question is %v, answer = %v, of the type %T %T\n", data[0], data[1], data[0], data[1])
+		insertQuestions(probMap, data)
 	}
-	fmt.Println(problemMap["1+1"] + problemMap["1+2"])
+}
+
+func displayQuestions(probMap map[string]int, correctCount *int) {
+	for k, answer := range probMap {
+		var userVal int
+		fmt.Printf("What does %v equal?\n", k)
+		_, err := fmt.Scanln(&userVal)
+		if err != nil {
+			fmt.Println("Invalid Input, input must be a number", err)
+			return
+		}
+		if userVal == answer {
+			*correctCount++
+		}
+
+		if *correctCount >= 10 {
+			fmt.Printf("Well done you got %v answers correct!", *correctCount)
+			break
+		}
+	}
+}
+
+func main() {
+	var correctAnswers int
+	readInFileData(problemMap)
+	displayQuestions(problemMap, &correctAnswers)
+	fmt.Println("*** End of File ***")
 }
